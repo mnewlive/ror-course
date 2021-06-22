558291c35cacbb845563e42b49e4bfe25cf54c48
mnewlive <morozovvadim91@gmail.com>
2021-06-22 23:20:05 +0300

    Added navigation compose

--- app/src/main/java/com/mandarine/ror/course/MainActivity.kt
+++ app/src/main/java/com/mandarine/ror/course/MainActivity.kt
@@ -3,34 +3,15 @@ package com.mandarine.ror.course
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
-import androidx.compose.material.Surface
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navArgument
+import androidx.navigation.compose.rememberNavController
 import androidx.compose.runtime.Composable
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.transform.CircleCropTransformation
-import com.google.accompanist.coil.rememberCoilPainter
 import com.mandarine.ror.course.data.MockSessions
+import com.mandarine.ror.course.navigation.Destinations
+import com.mandarine.ror.course.screens.SessionDetailsScreen
 import com.mandarine.ror.course.ui.RorTheme
 
 class MainActivity : ComponentActivity() {
@@ -38,10 +19,42 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            RorTheme {
+            MainNavigation()
+        }
+    }
+}
+
+@Composable
+private fun MainNavigation() {
+    val navController = rememberNavController()
+    RorTheme {
+        NavHost(
+            navController = navController,
+            startDestination = Destinations.Main,
+        ) {
+            composable(Destinations.Main) {
                 MainScreen(
+                    sessions = MockSessions,
                     favoriteSessions = MockSessions.take(3),
-                    sessions = MockSessions
+                    onSessionClick = { sessionId ->
+                        navController.navigate(
+                            "${Destinations.SessionDetails}/$sessionId"
+                        )
+                    }
+                )
+            }
+            composable(
+                "${Destinations.SessionDetails}/{${Destinations.SessionDetailsArgs.SessionId}}",
+                arguments = listOf(
+                    navArgument(Destinations.SessionDetailsArgs.SessionId) {
+                        type = NavType.StringType
+                    }
+                )
+            ) { backStackEntry ->
+                val sessionId = backStackEntry.arguments?.getString(Destinations.SessionDetailsArgs.SessionId)
+                val session = MockSessions.first { it.id == sessionId }
+                SessionDetailsScreen(
+                    session = session,
                 )
             }
         }
---
+++ app/src/main/java/com/mandarine/ror/course/navigation/Destinations.kt
@@ -0,0 +1,12 @@
+package com.mandarine.ror.course.navigation
+
+object Destinations {
+
+    const val Main = "main"
+
+    const val SessionDetails = "sessionDetails"
+
+    object SessionDetailsArgs {
+        const val SessionId = "sessionId"
+    }
+}
